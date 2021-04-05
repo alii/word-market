cce4b241359cf7040f6044752128905eb20b6ed6
Alistair Smith <alistairsmith01@gmail.com>
2021-04-05 20:23:28 +0100

    my humour has dropped off

--- src/commands/util.ts
+++ src/commands/util.ts
@@ -1,11 +1,15 @@
 import {Command} from "../types/command";
+import {StandardEmbed} from "../structs/standard-embed";
 
 export const util: Command = {
   aliases: ["ping", "pi"],
   description: "Checks that the bot is online",
-  syntax: "<message>",
   inhibitors: [],
-  async run(message, args) {
-    await message.reply(args.join(" "));
+  async run(message) {
+    const start = Date.now();
+    const m = await message.channel.send(new StandardEmbed(message.author).setTitle("Pong!"));
+    const end = Date.now() - start;
+
+    await m.edit(new StandardEmbed(message.author).setTitle(`Pong! API Latency ${end}ms`));
   },
 };
